0c6cf411f3356de2145ff120fe059c52d24b959a
Vivek Tiwari <vivekt123456@gmail.com>
2024-05-27 01:22:43 +0530

    ....

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: isProd ? '/Resume' : '',
-  assetPrefix: isProd ? '/Resume/' : '',
+  basePath: '/Resume',
+  assetPrefix: '/Resume/',
   trailingSlash: true,
   output: 'export',
   images: {
