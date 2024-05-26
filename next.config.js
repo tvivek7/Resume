7f0cd1bdc126393f1c9f8b15c95e787e539ebd9e
Vivek Tiwari <vivekt123456@gmail.com>
2024-05-27 00:49:24 +0530

    build settings

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: isProd ? '/Resume' : '',
+  assetPrefix: isProd ? '/Resume/' : '',
 }
 
 module.exports = nextConfig
