2e90795bd569a227ad016847e2b78eba016187d0
Vivek Tiwari <vivekt123456@gmail.com>
2024-06-02 01:29:41 +0530

    dges

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: isProd ? '/Resume' : '',
-  assetPrefix: isProd ? '/Resume/' : '',
+  basePath: '',
+  assetPrefix:'',
   trailingSlash: true,
   output: 'export',
   images: {
